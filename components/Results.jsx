7be7a6826b9d0733e2722d1dd1b07c4320675971
Rafael Willen <rafaelpadre20@gmail.com>
2022-03-08 13:33:24 +0100

    ✨ fetch data from the TMDB API

---
+++ components/Results.jsx
@@ -0,0 +1,5 @@
+function Results({ results }) {
+  return <div>Results</div>;
+}
+
+export default Results;
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,8 +1,10 @@
 import Head from "next/head";
 import Header from "../components/Header";
 import Nav from "../components/Nav";
+import Results from "../components/Results";
+import requests from "../utils/requests";
 
-export default function Home() {
+export default function Home({ results }) {
   return (
     <div>
       <Head>
@@ -12,6 +14,22 @@ export default function Home() {
 
       <Header />
       <Nav />
+      <Results results={results} />
     </div>
   );
+}
+
+export async function getServerSideProps(context) {
+  const genre = context.query.genre;
+
+  const results = await fetch(
+    `https://api.themoviedb.org/3${
+      requests[genre]?.url || requests.fetchTrending.url
+    }`
+  ).then((result) => result.json());
+  return {
+    props: {
+      results,
+    },
+  };
 }
