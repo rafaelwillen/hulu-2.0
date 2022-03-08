1261cef211316291b166e4b9225b503dc681c9ed
Rafael Willen <rafaelpadre20@gmail.com>
2022-03-08 12:22:19 +0100

    ✨ create the HeaderItem

--- components/HeaderItem.jsx
+++ components/HeaderItem.jsx
@@ -2,9 +2,15 @@ import React from "react";
 
 function HeaderItem({ title, Icon }) {
   return (
-    <li>
-      <Icon />
-      <p>{title}</p>
+    //   Adds group
+    <li className="group flex flex-col items-center cursor-pointer w-12 sm:w-20 hover:text-white ">
+      {/* Adds height of 6 (1.5rem) and margin bottom */}
+      <Icon className="h-6 mb-1 group-hover:animate-bounce" />
+      {/* Adds more space between the letters */}
+      {/* When the group (li) is hovered the p will apply the hover state */}
+      <p className="tracking-widest opacity-0 group-hover:opacity-100">
+        {title}
+      </p>
     </li>
   );
 }
